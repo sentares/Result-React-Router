b2126fded0670db419ef964e91c195e30d1d40c4
sentares <aydararzybekov2002@mail.ru>
2025-06-03 17:01:42 +0600

    feat: LocationsList map items

--- src/pages/locations/list/LocationsList.tsx
+++ src/pages/locations/list/LocationsList.tsx
@@ -1,3 +1,24 @@
+import { internalPaths } from '@/app/router/RoutePaths'
+import { Locations } from '@/data'
+import { Link, useLocation } from 'react-router-dom'
+
 export function LocationsList() {
-	return <div>LocationsList</div>
+	const { pathname } = useLocation()
+	const category = pathname.split('/')[1]
+
+	return (
+		<div>
+			<h1 className='category'>{category.toUpperCase()}</h1>
+
+			<ul>
+				{Locations.map(item => (
+					<li key={item.id}>
+						<Link to={internalPaths.locations.detail(String(item.id))}>
+							{item.name}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
