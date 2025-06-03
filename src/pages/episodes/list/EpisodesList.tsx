0b97382d329f5f58eb7156eb1a3f364f4ba394f0
sentares <aydararzybekov2002@mail.ru>
2025-06-03 17:01:30 +0600

    feat: EpisodesList map items

--- src/pages/episodes/list/EpisodesList.tsx
+++ src/pages/episodes/list/EpisodesList.tsx
@@ -1,3 +1,24 @@
+import { internalPaths } from '@/app/router/RoutePaths'
+import { Episodes } from '@/data'
+import { Link, useLocation } from 'react-router-dom'
+
 export function EpisodesList() {
-	return <div>EpisodesList</div>
+	const { pathname } = useLocation()
+	const category = pathname.split('/')[1]
+
+	return (
+		<div>
+			<h1 className='category'>{category.toUpperCase()}</h1>
+
+			<ul>
+				{Episodes.map(item => (
+					<li key={item.id}>
+						<Link to={internalPaths.episodes.detail(String(item.id))}>
+							{item.name}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
