247b5a557e587ba23a7a0a3eb7ad5c667c78a326
sentares <aydararzybekov2002@mail.ru>
2025-06-03 16:38:43 +0600

    feat: add LocationsList and LocationDetail pages to Router

--- src/app/router/Router.tsx
+++ src/app/router/Router.tsx
@@ -1,10 +1,12 @@
+import { CharacterDetail } from '@/pages/characters/detail'
+import { CharactersList } from '@/pages/characters/list'
 import { Home } from '@/pages/home'
+import { LocationDetail } from '@/pages/locations/detail'
+import { LocationsList } from '@/pages/locations/list'
 import { NotFound } from '@/pages/not-found'
 import { createBrowserRouter } from 'react-router-dom'
 import Layout from '../layouts/Layout'
 import { internalPaths } from './RoutePaths'
-import { CharactersList } from '@/pages/characters/list'
-import { CharacterDetail } from '@/pages/characters/detail'
 
 const Router = createBrowserRouter([
 	{
@@ -23,6 +25,20 @@ const Router = createBrowserRouter([
 					{
 						path: ':id',
 						element: <CharacterDetail />,
+					},
+				],
+			},
+
+			{
+				path: internalPaths.locations.list,
+				children: [
+					{
+						index: true,
+						element: <LocationsList />,
+					},
+					{
+						path: ':id',
+						element: <LocationDetail />,
 					},
 				],
 			},
---
+++ src/pages/locations/detail/LocationDetail.tsx
@@ -0,0 +1,3 @@
+export function LocationDetail() {
+	return <div>LocationDetail</div>
+}
