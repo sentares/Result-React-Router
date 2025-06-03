87130f9880e91c27fbb32726e345b00b019fc41e
sentares <aydararzybekov2002@mail.ru>
2025-06-03 17:01:15 +0600

    feat: CharactersList map items

--- src/pages/characters/list/CharactersList.tsx
+++ src/pages/characters/list/CharactersList.tsx
@@ -1,3 +1,24 @@
+import { internalPaths } from '@/app/router/RoutePaths'
+import { Characters } from '@/data'
+import { Link, useLocation } from 'react-router-dom'
+
 export function CharactersList() {
-	return <div>CharacterList</div>
+	const { pathname } = useLocation()
+	const category = pathname.split('/')[1]
+
+	return (
+		<div>
+			<h1 className='category'>{category.toUpperCase()}</h1>
+
+			<ul>
+				{Characters.map(item => (
+					<li key={item.id}>
+						<Link to={internalPaths.characters.detail(String(item.id))}>
+							{item.name}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
