13dff5d18395bba43ce40a81658e3d7270849d8a
sentares <aydararzybekov2002@mail.ru>
2025-06-03 16:14:21 +0600

    feat: add links

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,18 @@
+import { Link } from 'react-router-dom'
+import styles from './Home.module.scss'
+import { internalPaths } from '@/app/router/RoutePaths'
+
 export function Home() {
 	return (
-		<div>
+		<div className={styles.home}>
 			<h1>Добро пожаловать во вселенную Рика и Морти!</h1>
 			<p>Выберите категорию в навигации</p>
+
+			<div className={styles.links}>
+				<Link to={internalPaths.characters.list}>Characters</Link>
+				<Link to={internalPaths.locations.list}>Locations</Link>
+				<Link to={internalPaths.episodes.list}>Episodes</Link>
+			</div>
 		</div>
 	)
 }
