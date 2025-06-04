2cae9175df751df1b8a6f2be76b06ab39b64ecff
sentares <aydararzybekov2002@mail.ru>
2025-06-04 17:40:06 +0600

    feat: create logic of login and save token in storage

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,3 +1,125 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { generateToken } from '@/core/helpers/token'
+import { isEmail, minLength } from '@/core/helpers/validate'
+import { useLocalStorage } from '@/core/hooks'
+import { IconAt, IconEye, IconEyeOff, IconLock } from '@tabler/icons-react'
+import { useRef, useState, type ChangeEvent, type FormEvent } from 'react'
+import { useNavigate } from 'react-router-dom'
+import styles from './Login.module.scss'
+
+export interface LoginOptions {
+	email: string
+	password: string
+}
+
 export function Login() {
-	return <>Login</>
+	const navigate = useNavigate()
+	const formRef = useRef<HTMLFormElement>(null)
+
+	const [inputs, setInputs] = useState<LoginOptions>({
+		email: '',
+		password: '',
+	})
+	const [errors, setErrors] = useState<Partial<LoginOptions>>({})
+	const [showPassword, setShowPassword] = useState(false)
+	const [token, { setItem: saveToken }] = useLocalStorage('auth_token')
+
+	const handleChange = (event: ChangeEvent<HTMLFormElement>) => {
+		const target = event.target as HTMLElement
+
+		if (target instanceof HTMLInputElement && target.name) {
+			setInputs(prev => ({
+				...prev,
+				[target.name]: target.value,
+			}))
+
+			setErrors(prev => ({
+				...prev,
+				[target.name]: '',
+			}))
+		}
+	}
+
+	const validate = () => {
+		const newErrors: Partial<LoginOptions> = {
+			email: isEmail(inputs.email),
+			password: minLength(inputs.password, 6),
+		}
+
+		setErrors(newErrors)
+
+		return !Object.values(newErrors).some(Boolean)
+	}
+
+	const handleReset = () => {
+		setInputs({ email: '', password: '' })
+		setErrors({})
+	}
+
+	const togglePasswordVisibility = () => setShowPassword(prev => !prev)
+
+	const onSubmit = (inputs: LoginOptions) => {
+		const token = generateToken(inputs.email)
+		saveToken(token)
+		navigate('/')
+	}
+
+	const handleSubmit = (event: FormEvent) => {
+		event.preventDefault()
+
+		if (!validate()) return
+
+		formRef.current?.reset()
+		onSubmit(inputs)
+	}
+
+	return (
+		<div className={styles.Page}>
+			<div className={styles.Login}>
+				<form
+					className={styles.LoginForm}
+					ref={formRef}
+					onSubmit={handleSubmit}
+					onChange={handleChange}
+					onReset={handleReset}
+				>
+					<Input
+						withAsterisk
+						label='Введите почту'
+						description='Обязательно заполните поле'
+						name='email'
+						type='email'
+						leftSection={<IconAt size={16} />}
+						placeholder='Введите email'
+						required
+						error={errors.email}
+					/>
+
+					<Input
+						withAsterisk
+						label='Введите пароль'
+						description='Обязательно заполните поле'
+						name='password'
+						type={showPassword ? 'text' : 'password'}
+						leftSection={<IconLock size={16} />}
+						rightSection={
+							showPassword ? (
+								<IconEyeOff size={16} onClick={togglePasswordVisibility} />
+							) : (
+								<IconEye size={16} onClick={togglePasswordVisibility} />
+							)
+						}
+						placeholder='Введите пароль'
+						required
+						error={errors.password}
+					/>
+
+					<Button type='submit' variant='BACKGROUND'>
+						Войти
+					</Button>
+				</form>
+			</div>
+		</div>
+	)
 }
