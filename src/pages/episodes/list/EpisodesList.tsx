7864ed8fe96a59317ebfc0be0c05dd77364555d4
Aidar <aydararzybekov2002@mail.ru>
2025-06-22 15:12:15 +0600

    fix: use custom hook with api and fix search logic

--- src/pages/episodes/list/EpisodesList.tsx
+++ src/pages/episodes/list/EpisodesList.tsx
@@ -1,40 +1,90 @@
 import { internalPaths } from '@/app/router'
-import { DynamicComponent } from '@/components/common/dynamic'
+import { getDynamicComponent } from '@/components/common/dynamic'
 import { getCategoryFromPath } from '@/core/helpers/category'
-import { sortByCreated } from '@/core/helpers/sort'
-import { useSortParam } from '@/core/hooks'
-import { Episodes, type Episode } from '@/data'
-import { useMemo, type ComponentProps } from 'react'
+import { useDebounce } from '@/core/hooks'
+import { useGetEpisodes } from '@/core/hooks/api/episodes'
+import { type Episode } from '@/data'
+import { useCallback, useRef, useState, type ChangeEvent } from 'react'
 import { Link, useLocation } from 'react-router-dom'
+import styles from './EpisodesList.module.scss'
 
-const Sorting = (
-	props: Omit<ComponentProps<typeof DynamicComponent>, 'nameComponent'>
-) => <DynamicComponent nameComponent='Sorting' {...props} />
+const Input = getDynamicComponent('Input')
 
 export default function EpisodesList() {
 	const { pathname } = useLocation()
 	const category = getCategoryFromPath(pathname)
-	const sort = useSortParam()
 
-	const sortedData = useMemo(() => {
-		return sortByCreated(Episodes, sort)
-	}, [sort])
+	const observer = useRef<IntersectionObserver>(null)
+
+	const [pageNumber, setPageNumber] = useState(1)
+	const [inputSearch, setInputSearch] = useState('')
+	const [queryParams, setQueryParams] = useState({
+		search: '',
+	})
+
+	const { episodes, error, hasMore, loading, notFound } = useGetEpisodes(
+		queryParams,
+		pageNumber
+	)
+
+	const lastEpisodeRef = useCallback(
+		(node: HTMLLIElement) => {
+			if (loading) return
+			if (observer.current) observer.current.disconnect()
+
+			observer.current = new IntersectionObserver(entries => {
+				if (entries[0].isIntersecting && hasMore) {
+					setPageNumber(prevPageNumber => prevPageNumber + 1)
+				}
+			})
+
+			if (node) observer.current.observe(node)
+		},
+		[loading, hasMore]
+	)
+
+	useDebounce(
+		() => {
+			setQueryParams(prev => {
+				if (prev.search === inputSearch) return prev
+				setPageNumber(1)
+				return { ...prev, search: inputSearch }
+			})
+		},
+		500,
+		[inputSearch]
+	)
+
+	const handleChangeInput = (event: ChangeEvent<HTMLInputElement>) => {
+		setInputSearch(event.target.value)
+	}
 
 	return (
 		<div>
-			<h1 className='category'>{category.toUpperCase()}</h1>
-
-			<Sorting />
+			<div className={styles.filterBar}>
+				<h1 className='category'>{category.toUpperCase()}</h1>
+				<Input onChange={handleChangeInput} />
+			</div>
 
 			<ul>
-				{sortedData.map((episode: Episode) => (
-					<li key={episode.id}>
-						<Link to={internalPaths.episodes.detail(String(episode.id))}>
-							{episode.name}
+				{episodes.map((item: Episode, index) => (
+					<li
+						key={item.id}
+						ref={index === episodes.length - 5 ? lastEpisodeRef : null}
+					>
+						<Link to={internalPaths.episodes.detail(String(item.id))}>
+							{item.name}
 						</Link>
 					</li>
 				))}
 			</ul>
+
+			{loading && <div>Loading more episodes...</div>}
+			{error && <div>Error loading episodes</div>}
+			{notFound && !loading && <div>No episodes found for your search.</div>}
+			{!hasMore && !loading && !error && !notFound && (
+				<div>No more episodes to load</div>
+			)}
 		</div>
 	)
 }
