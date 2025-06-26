6ac876e1e23ee4ec1f5bcdbbfe53cfdd443a49b2
sentares <aydararzybekov2002@mail.ru>
2025-06-26 14:56:24 +0600

    feat: add sw.js

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from 'react-dom/client'
 import App from './app/App'
 import './app/styles/index.scss'
 
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
+
 createRoot(document.getElementById('root')!).render(<App />)
